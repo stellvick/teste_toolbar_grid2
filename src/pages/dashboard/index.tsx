505882753d52a0cc3ec5ec1b3020d3554c8c612b
Victor Costa <vitao.prieto@gmail.com>
2024-09-05 14:53:10 -0300

    teste

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,6 +4,9 @@ import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
 import Button from '@mui/material/Button';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import dayjs from 'dayjs';
+import { Card, CardContent, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import BaseCard from '../../components/base';
 
 const Dashboard = () => {
   function PageToolbar() {
@@ -23,7 +26,36 @@ const Dashboard = () => {
     );
   }
   return (
-    <PageContainer slots={{ toolbar: PageToolbar }}>Page content</PageContainer>
+    <PageContainer slots={{ toolbar: PageToolbar }}>
+      <BaseCard>
+      <Grid container spacing={3}>
+        <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="caption">Temperature</Typography>
+              <Typography variant="h4">24°C</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="caption">Precipitation</Typography>
+              <Typography variant="h4">5%</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="caption">Wind</Typography>
+              <Typography variant="h4">18km/h</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+      </BaseCard>
+    </PageContainer>
   );
 };
 
