3b69e2222478bb4fba9385d2d5bfa80f052d53fa
Victor Costa <vitao.prieto@gmail.com>
2024-09-05 07:29:44 -0300

    location provider

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,17 +2,21 @@ import { AppProvider } from '@toolpad/core/AppProvider';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { Outlet } from 'react-router-dom';
 import { BASETHEME, NAVIGATION } from '../../utils/constants';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
 
 export default function DashboardLayoutBasic() {
 
   return (
-    <AppProvider
-      navigation={NAVIGATION}
-      theme={BASETHEME}
-    >
-      <DashboardLayout>
-        <Outlet />
-      </DashboardLayout>
-    </AppProvider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <AppProvider
+        navigation={NAVIGATION}
+        theme={BASETHEME}
+      >
+        <DashboardLayout>
+          <Outlet />
+        </DashboardLayout>
+      </AppProvider>
+    </LocalizationProvider>
   );
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,28 +1,30 @@
-import { Button } from "@mui/material";
-import { PageContainer, PageContainerToolbar } from "@toolpad/core";
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import { PageContainer, PageContainerToolbar } from '@toolpad/core/PageContainer';
+import { SingleInputDateRangeField } from '@mui/x-date-pickers-pro/SingleInputDateRangeField';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-import { SingleInputDateRangeField } from "@mui/x-date-pickers-pro";
+import Button from '@mui/material/Button';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import dayjs from 'dayjs';
 
 const Dashboard = () => {
-    function PageToolbar() {
-        return (
-          <PageContainerToolbar>
-            <Button startIcon={<FileDownloadIcon />} color="inherit">
-              Export
-            </Button>
-            <DateRangePicker
-              sx={{ width: 220 }}
-              slots={{ field: SingleInputDateRangeField }}
-              slotProps={{ field: { size: 'small' } as any }}
-              label="Period"
-            />
-          </PageContainerToolbar>
-        );
-      }
+  function PageToolbar() {
     return (
-        <PageContainer slots={{ toolbar: PageToolbar }}>Page content</PageContainer>
+      <PageContainerToolbar>
+        <Button startIcon={<FileDownloadIcon />} color="inherit">
+          Export
+        </Button>
+        <DateRangePicker
+          sx={{ width: 220 }}
+          defaultValue={[dayjs(), dayjs().add(14, 'day')]}
+          slots={{ field: SingleInputDateRangeField }}
+          slotProps={{ field: { size: 'small' } as any }}
+          label="Period"
+        />
+      </PageContainerToolbar>
     );
+  }
+  return (
+    <PageContainer slots={{ toolbar: PageToolbar }}>Page content</PageContainer>
+  );
 };
 
 export default Dashboard;
